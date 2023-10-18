a03d3d4c58cdbc43431e2eff071f477e33fc421a
Phuong Dev <phuongdev89@gmail.com>
2023-10-18 15:50:26 +0000

    add `tracePathMappings` & `useDocker`

--- readme.md
+++ readme.md
@@ -1,5 +1,4 @@
 
-# Installation #
 ## Dependent ##
 Must have [phpstorm-protocol](https://github.com/phuongdev89/phpstorm-protocol) before
 
@@ -19,13 +18,35 @@ or add
 ```
 
 to the require section of your `composer.json` file.
-
-# Configuration #
+## Parameters ##
+#### $traceLine
+Type: `string`  
+Default: `{html}`  
+If [phpstorm-protocol](https://github.com/phuongdev89/phpstorm-protocol) installed, change it to
+```
+<a href="phpstorm://open?url=file://{file}&line={line}">{html}</a>
+```
+#### $useDocker
+Type: `bool`  
+Default: `false`  
+If you are using docker to deploy this project, set it `true`
+#### $tracePathMappings
+Type: `array|null`  
+Default: `null`  
+In docker container, project directory is `/app` maybe different with your IDE, just map it
+## Example ##
 
 ```
     'components' => [
         'errorHandler' => [
             'class' => \phuongdev89\errorhandler\ErrorHandler::class,
+            /**
+            'traceLine' => '<a href="phpstorm://open?url=file://{file}&line={line}">{html}</a>',
+            'useDocker' => true,
+            'tracePathMappings' => [
+                '/app' => 'D:\\Ampps\\www\mofiex-com'
+            ]
+            */
         ], 
     ],
 ```
--- src/ErrorHandler.php
+++ src/ErrorHandler.php
@@ -1,10 +1,66 @@
 <?php
 
 namespace phuongdev89\errorhandler;
+use yii\base\InvalidConfigException;
+
 class ErrorHandler extends \yii\web\ErrorHandler
 {
 
-    public $callStackItemView = '@phuongdev89/errorhandler/views/callStackItem.php';
-
+    /**
+     * @inheritdoc
+     */
     public $errorAction = 'site/error';
+
+    /**
+     * If you are using docker to deploy this project, set it "true"
+     * Default is "false"
+     * @var bool
+    */
+    public $useDocker = false;
+
+    /**
+     * In docker container, project directory is "/app" maybe different with your IDE, just map it
+     * Only effect if $useDocker is "true"
+     * @example ['/app' => 'D:\\Ampps\\www\\project']
+     * @var array
+    */
+    public $tracePathMappings = [];
+
+    /**
+     * @inheritDoc
+    */
+    public function renderCallStackItem($file, $line, $class, $method, $args, $index)
+    {
+        $lines = [];
+        $begin = $end = 0;
+        if ($file !== null && $line !== null) {
+            $line--; // adjust line number from one-based to zero-based
+            $lines = @file($file);
+            if ($line < 0 || $lines === false || ($lineCount = count($lines)) < $line) {
+                return '';
+            }
+
+            $half = (int) (($index === 1 ? $this->maxSourceLines : $this->maxTraceSourceLines) / 2);
+            $begin = $line - $half > 0 ? $line - $half : 0;
+            $end = $line + $half < $lineCount ? $line + $half : $lineCount - 1;
+        }
+
+        if ($this->useDocker) {
+            foreach ($this->tracePathMappings as $source => $target) {
+                $file = str_replace($source, $target, $file);
+            }
+        }
+
+        return $this->renderFile($this->callStackItemView, [
+            'file' => $file,
+            'line' => $line,
+            'class' => $class,
+            'method' => $method,
+            'index' => $index,
+            'lines' => $lines,
+            'begin' => $begin,
+            'end' => $end,
+            'args' => $args,
+        ]);
+    }
 }
